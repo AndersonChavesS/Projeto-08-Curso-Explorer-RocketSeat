7c19e83703ff93bcdae669235d879be95674b983
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-08-22 12:42:51 -0300

    refactor: modularizing the popup modal

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,19 +1,8 @@
+import { Modal } from './modal.js';
+
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-
-  open() {
-    Modal.wrapper.classList.add('open');
-  },
-  close() {
-    Modal.wrapper.classList.remove('open');
-  },
-};
 
 form.onsubmit = (e) => {
   e.preventDefault();
@@ -25,8 +14,6 @@ form.onsubmit = (e) => {
   Modal.message.innerText = message;
   Modal.open();
 };
-
-Modal.buttonClose.onclick = () => Modal.close();
 
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(1);
